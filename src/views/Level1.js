41d87ca062905b3c2267f2e1d20f7c7872078350
Maczi01 <mateusz.w.twardy@gmail.com>
2020-10-05 00:31:06 +0200

    add little window for first view

--- src/views/Level1.js
+++ src/views/Level1.js
@@ -3,7 +3,13 @@ import React, {Component} from 'react'
 class Level1 extends Component {
     render() {
         return (
-            <div>
+            <div style={
+                {padding: "1rem" , border: "1px solid gray", borderRadius: "5px",maxWidth: "400px", margin: '3rem auto'}
+            }>
+                <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                    <h2>LEVEL 1</h2>
+                    <h2>1/5</h2>
+                </div>
                 Level1
 
             </div>
